06acbe5f78b043f535ec8de99552d3c55e0dcc2a
Branel <emirpod02@gmail.com>
2024-03-02 12:08:05 +0100

    int

--- src/main/java/branel/org/example/factions/Factions.java
+++ src/main/java/branel/org/example/factions/Factions.java
@@ -6,11 +6,13 @@ public final class Factions extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        System.out.println("Factions is enabled");
         // Plugin startup logic
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        System.out.println("Faction is disabled");
     }
 }
